223776aa60cb01cf7bb3f3249f401630b966d7d5
Andrew <drace.davidson@gmail.com>
2023-05-01 21:38:57 -0600

    assignment 3 done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction One = new Fraction();
+        Console.WriteLine(One.GetFractionString());
+        Console.WriteLine(One.GetDecimalValue());
+
+        Fraction whole = new Fraction(6);
+        Console.WriteLine(whole.GetFractionString());
+        Console.WriteLine(whole.GetDecimalValue());
+
+        Fraction frac = new Fraction(1, 2);
+        Console.WriteLine(frac.GetFractionString());
+        Console.WriteLine(frac.GetDecimalValue());
+
+        Fraction doom = new Fraction(2, 8);
+        Console.WriteLine(doom.GetFractionString());
+        Console.WriteLine(doom.GetDecimalValue());
     }
 }
--- prove/Develop02/SaveJEntry.cs
+++ prove/Develop02/SaveJEntry.cs
@@ -1,16 +1,17 @@
 using System.IO;
-
-using (StreamWriter outputFile = new StreamWriter(_Filename))
-{
-    outputFile.WriteLine($"{_Date}");
-    outputFile.WriteLine();
-    outputFile.WriteLine($"{_Entry}, ");
-}
-
+using Streamwriter
 
 public class SaveJEntry
 {
     public string _Filename;
     public string _Date;
     public string _Entry;
+
+    public void FileWriter
+    {
+        StreamWriter outputFile = new StreamWriter(this._Filename);
+        outputFile.WriteLine($"{this._Date}");
+        outputFile.WriteLine();
+        outputFile.WriteLine($"{this._Entry}, ");
+    }
 }
