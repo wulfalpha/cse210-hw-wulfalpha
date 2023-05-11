4d73e357b6b1718ab8581b4c51b8a0768475acdc
Andrew <drace.davidson@gmail.com>
2023-05-10 18:44:32 -0600

    Added week 4 prepare

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+
+        WritingAssignment a4 = new WritingAssignment("Tod Smith", "Natural History", "The Rise of the Hominins");
+        Console.WriteLine(a4.GetSummary());
+        Console.WriteLine(a4.GetWritingInformation());
     }
 }
